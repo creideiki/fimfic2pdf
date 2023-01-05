2266ee4cd8c9e09fc60dfa7d0959870cfbf645ba
Karl-Johan Karlsson <creideiki@ferretporn.se>
2023-01-05 22:01:04 +0100

    Rename file anthology-renderer.rb to anthology_renderer.rb
    Rubocop dislikes hyphens in file names.

--- lib/fimfic2pdf.rb
+++ lib/fimfic2pdf.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 require 'fimfic2pdf/anthology'
-require 'fimfic2pdf/anthology-renderer'
+require 'fimfic2pdf/anthology_renderer'
 require 'fimfic2pdf/downloader'
 require 'fimfic2pdf/renderer'
 require 'fimfic2pdf/transformer'
