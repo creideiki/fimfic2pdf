839c668cc23c259397a80fff38bd4bdf539d47ee
Karl-Johan Karlsson <creideiki@ferretporn.se>
2023-12-30 23:27:35 +0100

    Move image downloading to FiMFic class
    AO3 ships images in the epub, so no downloading is required there.

--- lib/fimfic2pdf/fimfic/transformer.rb
+++ lib/fimfic2pdf/fimfic/transformer.rb
@@ -46,6 +46,37 @@ module FiMFic2PDF
 
         node.children[3..].map { |c| visit(c, file) }
       end
+
+      def download_image(url)
+        # We need a supported file name extension, or \includegraphics{}
+        # doesn't work. But it doesn't care if the data format matches
+        # the extension, and we don't know the data format until we've
+        # asked the server, which we want to avoid if possible. So just
+        # call everything .png.
+
+        # Also, FiMFiction runs all external references through a URL
+        # proxy, making it very difficult to determine the actual file
+        # name. Just use the hex MD5 of the URL and hope there are no
+        # collisions.
+        filename = 'img-' + Digest::MD5.hexdigest(url.split('/')[-1]) + '.png' # rubocop:disable Style/StringConcatenation
+        path = @options.id + File::SEPARATOR + filename
+
+        if File.exist? path
+          @logger.debug "Image #{url} already downloaded"
+        else
+          @logger.info "Fetching image from #{url}"
+          response = HTTParty.get(url,
+                                  headers: {
+                                    'User-Agent' => "FiMFic2PDF/#{FiMFic2PDF::VERSION}"
+                                  })
+          raise "Failed to download image #{url}: #{response.code} #{response.message}" unless response.code == 200
+
+          @logger.debug "Saving image as: #{path}"
+          File.binwrite(path, response.body)
+        end
+
+        filename
+      end
     end
   end
 end
